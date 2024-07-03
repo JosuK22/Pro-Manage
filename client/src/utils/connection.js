e11e7d23b320f691d9a9bb70566e1f7511136a63
JosuK22 <josephkanoj@gmail.com>
2024-07-03 20:18:19 +0530

    Final Touches

--- client/src/utils/connection.js
+++ client/src/utils/connection.js
@@ -1,4 +1,4 @@
-// const BACKEND_URL= 'https://temp-repo-kyo2.onrender.com'
-const BACKEND_URL= 'http://localhost:3003';
+const BACKEND_URL= 'https://pro-manage-xx4p.onrender.com'
+//const BACKEND_URL= 'http://localhost:3003';
 
 export {BACKEND_URL};
